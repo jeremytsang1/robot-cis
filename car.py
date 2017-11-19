e10043ba4e82028f7adb3610f00fa08c8e5a1bb7
Jeremy Tsang <jeremytsang1@gmail.com>
2017-11-19 00:46:03 -0800

    Create Car.point_turn()
    Create Car.point_turn() correctly to be a two motor operation. Motors
    run in opposing directions to turn the car.

--- car.py
+++ car.py
@@ -99,6 +99,43 @@ single motor to turn the car) or do an indefinite swing turn.
                     vertical_direction, turn_time)
                 sleep(wait_interval)
 
+    def point_turn(self, horizontal_direction,
+                   turn_time=-1, num_turns=1, wait_interval=.25):
+        """Causes the car to perform a series of point turns (operating both
+motors in opposite directions) or do an indefinite point turn.
+
+        Args:
+        horizontal_direction (int): Specfies side-to-side turning.
+          - -1: Turn left.
+          -  1: Turn right.
+        turn_time (float): Duration of each turn.
+          - nonnegative values: Duration of each turn.
+          - negative values: Turn indefinitely.
+        num_turns (int): Non negative int. Specifies how many turns to perform.
+        wait_interval (float): Delay between each turn.
+
+        Returns:
+        None
+
+        """
+        self.brake()  # make sure no unnecessary motors running
+        # turn_motor_lst
+        # -1: [1, 0] (right, left)
+        #  1: [0, 1] (left, right)
+        turn_motor_lst = self.motor_lst[::horizontal_direction]
+
+        if turn_time < 0:  # indefinite turning
+            turn_motor_lst[0].set_direction(1)
+            turn_motor_lst[1].set_direction(-1)
+        else:  # specified turn time
+            for i in range(num_turns):
+                turn_motor_lst[0].set_direction(1)
+                turn_motor_lst[1].set_direction(-1)
+                sleep(turn_time)
+                self.brake()
+                sleep(wait_interval)
+            self.brake()
+
 
 if __name__ == "__main__":
     print()
