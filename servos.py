7139b0bc75e1ab72e61700fe99993aedec398e2e
Jeremy Tsang <jeremytsang1@gmail.com>
2017-11-20 01:46:49 -0800

    Create ServoMotor class
    Create ServoMotor class for use in robotic arm.
    
    Pass all relevant physical information (channel connections, pulse
    length & angle calibrations, etc) through a dct (cals) during
    instantiation.
    
    Servos act finicky on the ArmUno 2.0 and may need a slight push from a
    finger as they tend to get stuck from the joints. This is necessary if
    they are producing a buzzing noise indicating that the joint is jammed
    and is currently struggling. Due to this fact, create
    ServoMotor.power_on() to have servo motors assume a preset position
    (ServoMotor.cals['pow_pl']) during initialization. This initial setting
    of position will be jerky as no current position (ServoMotor.current_pl)
    has been set at this point. Likewise make sure to run
    ServoMotor.power_off() at the end of each session to minimize jerking in
    the next session.
    
    Create ServoMotor.sweep() to allow broad sweeping motions of the servo
    horn. To avoid sudden jerking motion ServoMotor.current_pl is updated at
    each step and thus at the end of the function this current position
    attribute will be exactly where the desired ending position
    specified. Speed of rotation can be adjusted by modifying the
    pause_time parameter (smaller values lead to faster rotation).

---
+++ servos.py
@@ -0,0 +1,66 @@
+import time
+import Adafruit_PCA9685
+import config_log
+import logging
+
+
+class ServoMotor():
+    """Class for Servo Motors driven by PCA9685"""
+    number_of_motors = 0  # motors created in session (including destroyed)
+    instances = list()  # list of all the ServoMotor objects created
+
+    def __init__(self, name, channel, cals):
+        """
+        Args:
+        name (str): Name of servo motor.
+        cals (dct): dct containing calibration info for motor.
+
+        """
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.pwm = Adafruit_PCA9685.PCA9685()  # Pwm module for PCA9685
+        self.pwm.set_pwm_freq(60)  # Set servo period to 1/(60 s^-1)
+        self.name = name  # For convenience
+        self.cals = cals  # Pulse lengths determined in physical calibration
+        self.channel = self.cals["channel"]  # On PCA9685 board
+        self.num = ServoMotor.number_of_motors  # Identifier for servo
+
+        ServoMotor.number_of_motors += 1
+        ServoMotor.instances.append(self)
+
+        self.current_pl = self.cals['pow_pl']
+        self.power_on()
+
+    def sweep(self, end_pl, pause_time=0.005):
+        """Sweeps servo motor from the current position (with respect to pulse
+        length) to specified pulse length in unit
+        increments. Specified pauses are made between each increment.
+
+        Args:
+        end_pl (int): Pulse length to end at (between min_pl and max_pl)
+        pause_time (float): Time in seconds to wait between each jump.
+
+        Returns:
+        None
+
+        """
+        step = -1 if (end_pl - self.current_pl) < 0 else 1
+        while self.current_pl != end_pl:
+            self.current_pl += step
+            self.pwm.set_pwm(self.channel, 0, self.current_pl)
+            time.sleep(pause_time)
+
+    def power_on(self):
+        """Moves servo to the power on position. Make sure to always run
+        ServoMotor.power_off() before shutting off servos.
+
+        """
+        self.pwm.set_pwm(self.channel, 0, self.cals['pow_pl'])
+
+    def power_off(self):
+        """Sweeps servo to power on position to avoid violent startup."""
+        self.sweep(self.cals['pow_pl'])
+
+
+if __name__ == "__main__":
+    pass
