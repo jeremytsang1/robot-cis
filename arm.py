0f9bd317f34503657e1d682dc90f496e15a0252e
Jeremy Tsang <jeremytsang1@gmail.com>
2017-11-24 01:14:57 -0800

    Change dict name from cals to config
    Change to stay consistent with naming in other files.
    
    Write example configuration at end of file to prevent from running on
    import.

--- arm.py
+++ arm.py
@@ -6,18 +6,18 @@ import servos
 
 class Arm():
     """Class for arm of CARM."""
-    def __init__(self, cals):
-        """Args: cals (dict): Dict with configuration for each of the four
+    def __init__(self, config):
+        """Args: config (dict): Dict with configuration for each of the four
         servos that make up the arm.
 
         """
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-        self.gripper = servos.ServoMotor(cals['gripper'])
-        self.left = servos.ServoMotor(cals['left'])
-        self.right = servos.ServoMotor(cals['right'])
+        self.gripper = servos.ServoMotor(config['gripper'])
+        self.left = servos.ServoMotor(config['left'])
+        self.right = servos.ServoMotor(config['right'])
         # Disable base until finished with rest of arm.
-        # self.base = servos.ServoMotor(cals['base'])
+        # self.base = servos.ServoMotor(config['base'])
         # self.servo_list = (self.gripper, self.left, self.right, self.base)
         self.servo_list = (self.gripper, self.left, self.right)
 
@@ -27,8 +27,8 @@ class Arm():
         (open a closed gripper).
 
         """
-        dropoff = self.right.cals['min_pl']
-        pickup = self.right.cals['max_pl']
+        dropoff = self.right.config['min_pl']
+        pickup = self.right.config['max_pl']
         speed = 0.00005
         try:
             self.gripper.sweep(300, speed)
@@ -57,35 +57,40 @@ class Arm():
 
 if __name__ == "__main__":
     print()
-    cals = {
-        'gripper': {
-            'name': 'gripper',
-            'channel': 12,
-            'pow_pl': 300,
-            'min_pl': 135,
-            'max_pl': 300},
-        # Left and right servo are in opposition to each other. Until work
-        # out details keep left servo at 450 at all times.
-        'left': {  # use for up and down motion
-            'name': 'left',
-            'channel': 13,
-            'pow_pl': 450,
-            'min_pl': 150,
-            'max_pl': 450},
-        'right': {  # use for forward and backward motion
-            'name': 'right',
-            'channel': 14,
-            'pow_pl': 275,
-            'min_pl': 275,  # provided l.current_pl = 450
-            'max_pl': 550}
-        # 'base': {
-        #     'name': 'base',
-        #     'channel': 15,
-        #     'pow_pl': 400,
-        #     'min_pl': None,
-        #     'max_pl': None},
-    }
-    arm = Arm(cals)
+
+    gripper_servo_config = {
+        'name': 'gripper',
+        'channel': 12,
+        'pow_pl': 300,
+        'min_pl': 135,
+        'max_pl': 300}
+    left_servo_config = {  # use for up and down motion
+        'name': 'left',
+        'channel': 13,
+        'pow_pl': 450,
+        'min_pl': 150,
+        'max_pl': 450}
+    right_servo_config = {  # use for forward and backward motion
+        'name': 'right',
+        'channel': 14,
+        'pow_pl': 300,
+        'min_pl': 300,  # provided l.current_pl = 450
+        'max_pl': 550}
+    # base_servo_config = {
+    #     'name': 'base',
+    #     'channel': 15,
+    #     'pow_pl': 400,
+    #     'min_pl': None,
+    #     'max_pl': None},
+    # }
+
+    config = {
+        'gripper': gripper_servo_config,
+        'left': left_servo_config,
+        'right': right_servo_config
+        }
+    
+    arm = Arm(config)
 
     # A few grabs to make sure everything is working
     for i in range(3):
