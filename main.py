5f9d1f088ca6066d915e52493f92c3c6592ee805
Jeremy Tsang <jeremytsang1@gmail.com>
2017-11-30 00:41:19 -0800

    Restructure the menu and write line follow draft
    Restructure the menu not to use so many elif statements and to
    automatically align colons. Use a dict of tuples to hold all the actions
    for each option and divide menu if statement based on how many arguments
    each functionin the tuple takes. Also instead of having 'space' appear
    in the menu just indicate with ' '.
    
    Introducing line following concept. Write functions for the logic of
    each possible scenario of interrupting the IR sensors. Have position and
    motion dicts but don't actually use them at this point. Plan is to keep
    track of the robot to see where it needs to turn based on if it has
    passed the line and is trying to correct itself or not. Turning still
    needs work. Write while loop to cause the robot to check IR sensors
    every 0.1 seconds. Add option to menu to enter line following mode.
    
    Temporarily set logging level to INFO as checking every 0.1 seconds
    causes many logging messages to appear.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import logging
 import carm
 import config
 import RPi.GPIO as GPIO
+import time
 
 
 def stop():  # emergency stop
@@ -24,77 +26,128 @@ def manual_mode(robot):
 
 
     """
-    option = str()
-    menu_str = '\n'.join((
-        '',
-        '    w: forward',
-        '    s: backwards',
-        '    a: point left',
-        '    d: point left',
-        '    q: swing left FWD',
-        '    e: swing right FWD',
-        '    q: swing left BACK',
-        '    e: swing right BACK',
-        'space: brake',
-        '    g: grab',
-        '    o: opens',
-        '    n: full extend',
-        '    p: full retract',
-        '    [: increment extend',
-        '    ]: increment retract',        
-        '    1: look down',
-        '    2: look straight',
-        '    3: look up',
-        '    j: quit',
-        '\n> '))
+    option_key = str()
 
-    while option != 'j':
-        option = input(menu_str)
-        if option == 'w':
-            robot.car.drive(1)
-        elif option == 's':
-            robot.car.drive(-1)
-        elif option == 'a':
-            robot.car.point_turn(-1)
-        elif option == 'd':
-            robot.car.point_turn(1)
-        elif option == 'q':
-            robot.car.swing_turn(-1, 1)
-        elif option == 'e':
-            robot.car.swing_turn(1, 1)
-        elif option == 'z':
-            robot.car.swing_turn(-1, -1)
-        elif option == 'c':
-            robot.car.swing_turn(1, -1)
-        elif option == ' ':
-            robot.car.brake()
-        elif option == 'g':
-            robot.arm.close_gripper()
-        elif option == 'o':
-            robot.arm.open_gripper()
-        elif option == 'n':
-            robot.arm.right.sweep(robot.arm.right.config['max_pl'])
-        elif option == 'p':
-            robot.arm.right.sweep(robot.arm.right.config['min_pl'])
-        elif option == '[':
-            if robot.arm.right.current_pl > robot.arm.right.config['min_pl']:
-                robot.arm.extend(-5)
-        elif option == ']':
-            if robot.arm.right.current_pl < robot.arm.right.config['max_pl']:
-                robot.arm.extend(5)
-        elif option == '1':
-            robot.cam.lookdown()
-        elif option == '2':
-            robot.cam.power_off()
-        elif option == '3':
-            robot.cam.lookup()
-        elif option == 'j':
-            robot.car.brake()
-            robot.arm.poweroff()
-            robot.cam.power_off()
-            print("Goodbye!")
+    option_dct = {
+        'w': ('forwards', robot.car.drive, 1),
+        'a': ('point left', robot.car.drive, -1),
+        's': ('backwards', robot.car.point_turn, -1),
+        'd': ('point left', robot.car.point_turn, 1),
+        'e': ('swing right FWD', robot.car.swing_turn, -1, 1),  # 2 args
+        'q': ('swing left FWD', robot.car.swing_turn, 1, 1),  # 2 args
+        'z': ('swing left BACK', robot.car.swing_turn, -1, -1),  # 2 args
+        'c': ('swing right BACK', robot.car.swing_turn, 1, -1),  # 2 args
+        ' ': ('brake', robot.car.brake),  # 0 args
+        '[': ('increment extend', robot.arm.extend, -5),
+        ']': ('increment retract', robot.arm.extend, 5),
+        'g': ('grab', robot.arm.close_gripper),  # 0 args
+        'o': ('opens', robot.arm.open_gripper),  # 0 args
+        'n': ('full extend', robot.arm.right.sweep, robot.arm.right.config['max_pl']),
+        'p': ('full retract', robot.arm.right.sweep, robot.arm.right.config['min_pl']),
+        '1': ('look down', robot.cam.lookdown),  # 0 args
+        '2': ('look straight', robot.cam.power_off),  # 0 args
+        '3': ('look up', robot.cam.lookup),  # 0 args
+        'line': ('enter line following mode', line_following_mode, robot),
+        'j': ('quit',),
+    }
+    option_key_order = [
+        'w',
+        'a',
+        's',
+        'd',
+        'e',
+        'q',
+        'z',
+        'c',
+        ' ',
+        '[',
+        ']',
+        'g',
+        'o',
+        'n',
+        'p',
+        '1',
+        '2',
+        '3',
+        'line',
+        'j']
+
+    # aligning all colons in the menu
+    width = max([len(key) for key in option_dct.keys()])
+    menu_str = '\n'.join([ok.rjust(width) + ": " +
+                          option_dct[ok][0] for ok in option_key_order]) + '\n> '
+
+    while option_key != 'j':
+        print()
+        option_key = input(menu_str)
+        if option_key in option_dct.keys():
+            tup = option_dct[option_key]
+            if len(tup) == 2:
+                tup[1]()
+            elif len(tup) == 3:
+                tup[1](tup[2])
+            elif len(tup) == 4:
+                tup[1](tup[2], tup[3])
         else:
             print('Please choose a menu option\n')
+
+    # Shutdown
+    robot.car.brake()
+    robot.arm.poweroff()
+    robot.cam.power_off()
+    print("Goodbye!")
+
+
+def line_following_mode(robot):
+    def interrupt_neither(left, right):
+        return left and right
+
+    def interrupt_both(left, right):
+        return (not left) and (not right)
+
+    def interrupt_left(left, right):
+        return left and not right
+
+    def interrupt_right(left, right):
+        return right and not left
+
+    try:
+        robot.car.drive(1)
+        position = {'left_of_line': False,
+                    'right_of_line': False}
+        movement = {'brake': False,
+                    'turning_left': False,
+                    'turning_right': False,
+                    'driving_forward': False}
+
+        movement['brake'] = False
+        turning_left = False
+        turning_right = False
+        while not movement['brake']:
+            if interrupt_right(robot.irl.check(),
+                               robot.irr.check()) and not turning_left:
+                turning_left = True
+                robot.car.point_turn(-1)
+                time.sleep(2)
+            elif interrupt_right(robot.irl.check(),
+                                 robot.irr.check()) and turning_left:
+                turning_left = False
+                robot.car.swing_turn(1, 1)
+            elif interrupt_left(robot.irl.check(),
+                                robot.irr.check()) and not turning_right:
+                turning_right = True
+                robot.car.point_turn(1)
+                time.sleep(2)
+            elif interrupt_left(robot.irl.check(),
+                                robot.irr.check()) and turning_right:
+                turning_right = False
+                robot.car.swing_turn(-1, 1)
+            time.sleep(.1)  # delay to check sensors
+            movement['brake'] = interrupt_both(robot.irl.check(),
+                                               robot.irr.check())
+        robot.car.brake()
+    except:
+        robot.car.brake()
 
 
 if __name__ == "__main__":
