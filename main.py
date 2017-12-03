1faba4c09a4c5170d454098f13e9960eed71f1dd
Jeremy Tsang <jeremytsang1@gmail.com>
2017-12-03 03:52:37 -0800

    Rearrange order in dict, no effect on code

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def manual_mode(robot):
     option_dct = {
         'w': ('forwards', robot.car.drive, 1),
         's': ('backwards', robot.car.drive, -1),
-        'a': ('point left', robot.car.point_turn, -1),
-        'd': ('point right', robot.car.point_turn, 1),
-        'e': ('swing right FWD', robot.car.swing_turn, 1, 1),  # 2 args
-        'q': ('swing left FWD', robot.car.swing_turn, -1, 1),  # 2 args
+        'a': ('swing left FWD', robot.car.swing_turn, -1, 1),  # 2 args
+        'd': ('swing right FWD', robot.car.swing_turn, 1, 1),  # 2 args
+        'q': ('point left', robot.car.point_turn, -1),
+        'e': ('point right', robot.car.point_turn, 1),
         'z': ('swing left BACK', robot.car.swing_turn, -1, -1),  # 2 args
         'c': ('swing right BACK', robot.car.swing_turn, 1, -1),  # 2 args
         ' ': ('brake', robot.car.brake),  # 0 args
